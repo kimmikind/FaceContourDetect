d73aead815d2843170981a310ca739bf7026e364
Мария Самойлова <kimmikind@yandex.ru>
2025-02-12 20:48:11 +0300

    add face draw contour

---
+++ app/src/main/java/com/example/facecountourdetect/composables/CameraScreen.kt
@@ -0,0 +1,140 @@
+package com.example.facecountourdetect.composables
+
+import CameraPreview
+import android.util.Log
+import android.util.Size
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.util.concurrent.Executors
+
+@Composable
+fun CameraScreen() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+
+    var previewWidth by remember { mutableStateOf(0) }
+    var previewHeight by remember { mutableStateOf(0) }
+    var viewWidth by remember { mutableStateOf(0) }
+    var viewHeight by remember { mutableStateOf(0) }
+    var imageWidth by remember { mutableStateOf(0) }
+    var imageHeight by remember { mutableStateOf(0) }
+    // состояние для хранения списка распознанных лиц
+    var faces by remember { mutableStateOf<List<Face>>(emptyList()) }
+
+    // детектор лиц с высокой точностью
+    val options = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST) // высокая точность распознавания
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL) //  распознавание всех контуров лица
+        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL) // Распознавание ключевых точек (глаза, нос, рот)
+        .build()
+
+    // создаем клиент для распознавания лиц
+    val detector = FaceDetection.getClient(options)
+
+    // cоздаем состояние для хранения Future-объекта, который предоставляет доступ к ProcessCameraProvider.
+    var cameraProviderFuture by remember { mutableStateOf<ListenableFuture<ProcessCameraProvider>?>(null) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .onSizeChanged { size ->
+                viewWidth = size.width
+                viewHeight = size.height
+            }
+    ) {
+        CameraPreview(
+            modifier = Modifier.fillMaxSize(),
+            onPreviewViewCreated = { previewView ->
+                previewView.post {
+                    previewWidth = previewView.width
+                    previewHeight = previewView.height
+
+                }
+                previewView.scaleType = PreviewView.ScaleType.FIT_CENTER
+                cameraProviderFuture = ProcessCameraProvider.getInstance(previewView.context)
+                cameraProviderFuture?.addListener({
+                    val cameraProvider = cameraProviderFuture?.get()
+                    // Создаем Preview-объект, который будет отображать изображение с камеры.
+                    val preview = Preview.Builder().build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+                    // анализатор изображений для обработки кадров с камеры
+                    val imageAnalysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST) // Стратегия обработки только последнего кадра
+
+                        .build()
+                        .also { analyzer ->
+                            // устанавливаем анализатор
+                            analyzer.setAnalyzer(Executors.newSingleThreadExecutor()) { imageProxy ->
+                                imageWidth = imageProxy.width
+                                imageHeight = imageProxy.height
+                                Log.d("ImageDimensions", "Image dimensions: $imageWidth x $imageHeight")
+                                // получаем изображение
+                                val bitmap =
+                                    imageProxy.toBitmap() // Используем встроенный метод для преобразования
+                                if (bitmap != null) {
+                                    // создаем объект InputImage для передачи в детектор лиц
+                                    val image = InputImage.fromBitmap(
+                                        bitmap,
+                                        imageProxy.imageInfo.rotationDegrees
+                                    )
+                                    // обрабатываем изображение для распознавания лиц
+                                    detector.process(image)
+                                        .addOnSuccessListener { detectedFaces ->
+                                            // если лица распознаны, обновляем состояние faces
+                                            faces = detectedFaces
+                                        }
+                                        .addOnFailureListener { e ->
+                                            e.printStackTrace()
+                                        }
+                                }
+                                imageProxy.close()
+                            }
+                        }
+                    val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+                    try {
+                        cameraProvider?.unbindAll()
+                        // привязываем камеру к жизненному циклу и настраиваем Preview.
+                        cameraProvider?.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            preview,
+                            imageAnalysis
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }, ContextCompat.getMainExecutor(previewView.context)) // используем главный поток
+            }
+        )
+        DrawFaceContour(
+            previewWidth = previewWidth,
+            previewHeight = previewHeight,
+            viewWidth = viewWidth,
+            viewHeight = viewHeight,
+            imageWidth = imageWidth, // Ширина изображения (640)
+            imageHeight = imageHeight, // Высота изображения (480)
+            faces = faces)
+    }
+}
---
+++ app/src/main/java/com/example/facecountourdetect/composables/FaceContourDetect.kt
@@ -0,0 +1,83 @@
+package com.example.facecountourdetect.composables
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+
+@Composable
+fun FaceContourDetect() {
+    // контекст внутри Composable-функции.
+    val context = LocalContext.current
+
+    var hasCameraPermission by remember { mutableStateOf(false) }
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasCameraPermission = isGranted
+    }
+
+    // проверка при первом запуске
+    LaunchedEffect(Unit) {
+        val permissionCheckResult = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        )
+        if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+            hasCameraPermission = true
+        } else {
+            // запрос разрешения
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    // контейнер для отображения контента с использованием Material Design.
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        if (hasCameraPermission) {
+            CameraScreen()
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp), //  отступы по краям
+                verticalArrangement = Arrangement.Center, // выравниваем содержимое по центру
+                horizontalAlignment = Alignment.CenterHorizontally // выравниваем по горизонтали
+            ) {
+                Text(
+                    text =
+                    "The application requires access to the camera.",
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Button(
+                    onClick = { cameraPermissionLauncher.launch(Manifest.permission.CAMERA) },
+                    modifier = Modifier.wrapContentSize() // кнопка будет занимать только необходимую ей площадь
+                ) {
+                    Text("Request permission")
+                }
+            }
+        }
+    }
+}
